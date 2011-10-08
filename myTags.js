687abc85959af8afe3138b920beb999731e93ecd
Sonali Sridhar <sonali.sridhar@gmail.com>
2011-10-08 18:03:18 -0400

    myTags.js takes all tags from posts and displays unique tags.
    This uses an API key to grab all the post data, iterate through it
    displaying the unique ones in an <li> class.

---
+++ myTags.js
@@ -0,0 +1,20 @@
+jQuery.getJSON('http://api.tumblr.com/v2/blog/savesafe.tumblr.com/posts?api_key=5KShzHfHnwPMFxIsEDbdYgYsqIHeSENb7hJug8Mct6hVjguEMW&jsonp=?',
+function(data) {
+
+    var $elem;
+    $elem = $(".tag_list").first();
+
+    $.each(data.response.posts, function(idx, post) {
+        $.each(post.tags, function(idx, tag) {
+            if ($elem.find('a[href="/tagged/' + tag + '"]').length == 0) {
+                $elem.append(
+                    $('<li />').append(
+                        $('<a />')
+                            .attr('href', '/tagged/' + tag)
+                            .text(tag)
+                    )
+                );
+            }
+        });
+    });
+});
